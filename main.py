0b0880de015072c69df98258b31ab3db49ec8079
Andrey Rumyantsev <andreylrr@gmail.com>
2019-12-22 12:35:42 +0300

    After some debugging

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import hhrequest as hr
-import pprint as p
+import pickle as pic
 
 d_terms_dictionary = dict()
 s_url = 'https://api.hh.ru/vacancies'
@@ -12,9 +12,14 @@ o_hhrequest.set_search_pattern(s_search)
 l_urls = o_hhrequest.get_urls_vacancies()
 for s_url in l_urls:
     l_terms = o_hhrequest.process_url(s_url)
+    print(s_url)
     for s_term in l_terms:
         if s_term in d_terms_dictionary:
             d_terms_dictionary[s_term] += 1
         else:
             d_terms_dictionary[s_term] = 1
-    
+
+print(sorted(d_terms_dictionary.items(), key=lambda x: x[1], reverse=True))
+
+with open(s_search + '.pickle', 'wb') as f:
+    pic.dump(d_terms_dictionary, f)
